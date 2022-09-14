3850107626dce848bf699ecc0abd7fd5c3759090
Michael <michaelsousajr@gmail.com>
2022-09-14 03:51:17 -0400

    Added new files to start portfolio site

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+    arrowParents: 'avoid',
+    singleQuote: true,
+    bracketSpacing: true,
+    endOfLine: 'if',
+    semi: false,
+    tabWidth: 2,
+    tralingComma: 'none'
+}
+
+module.exports = options
